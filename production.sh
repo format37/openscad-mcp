57e5bfc49f532920f6eb7154be47564f53c7775e
Alex <format37@gmail.com>
2025-09-13 19:35:51 +0400

    daemon

--- production.sh
+++ production.sh
@@ -5,4 +5,4 @@ source .env.production
 
 sudo docker stop mcp-openscad-resource-server 2>/dev/null || true
 
-sudo docker compose -f docker-compose.production.yml --env-file .env.production up --build
+sudo docker compose -f docker-compose.production.yml --env-file .env.production up --build -d
