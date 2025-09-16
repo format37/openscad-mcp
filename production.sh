5c9d459800dd33bb9da12f97e7e68ae9b06fd3e9
Alex <format37@gmail.com>
2025-09-16 12:44:22 +0400

    removed legacy names

--- production.sh
+++ production.sh
@@ -3,6 +3,6 @@
 # Load environment variables
 source .env.production
 
-sudo docker stop mcp-openscad-resource-server 2>/dev/null || true
+sudo docker stop mcp-openscad 2>/dev/null || true
 
 sudo docker compose -f docker-compose.production.yml --env-file .env.production up --build -d
